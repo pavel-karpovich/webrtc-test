0a30c045ab4145f05ce84bb2b4f975b6ca35dde6
Pavel Karpovich <pavel.karpovich@brightpattern.com>
2023-01-17 22:31:08 +0300

    Add favicon.ico

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
@@ -15,6 +15,11 @@ module.exports = {
             template: path.resolve(__dirname, 'src', 'index.html'),
             filename: 'index.html',
             inject: true,
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: 'public', to: 'dist'},
+            ],
         }),
     ],
     mode: 'development',
